b4b7ee85dfb50c826055c58731ad9a4808aab30c
Anna Garcia <annaegarcia@gmail.com>
2016-04-27 17:53:56 -0400

    added morgan middleware

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-var express = require('express');
-var http = require('http');
-var app = express();
-var port = 3000;
+var express = require('express'),
+       http = require('http'),
+      chalk = require('chalk'),
+     morgan = require('morgan'),
 
-var server = http.createServer();
+        app = express(),
+       port = 3000,
+
+     server = http.createServer();
 
 server.on('request',app);
 
+// app.use(function(req, res, next){
+//   console.log(chalk.magenta(req.method), chalk.cyan(req.path), chalk.gray(res.statusCode));
+//   console.log(req);
+//   next();
+// });
+
+//This does what the above function does ^
+app.use(morgan('tiny'));
+
+app.use(/\/special.*/, function(req, res, next){
+  console.log(chalk.yellow("You are special"));
+  next();
+});
+
 app.get('/',function(req, res){
   res.send('Hello World!');
+});
+
+app.get('/news',function(req, res){
+  res.send('Today is Wednesday. We are at Grace Hopper Academy');
 });
 
 server.listen(port, function(){
