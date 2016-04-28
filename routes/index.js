b0c5e246829bfcf37283a46b1f671cbbb90f3143
Anna Garcia <annaegarcia@gmail.com>
2016-04-28 11:23:53 -0400

    route tweet by name and id

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,17 @@ router.get('/', function (req, res) {
 
 router.use(express.static('public'));
 
+router.get( '/users/:name', function(req, res, next){
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  console.log(list);
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
+});
+
+router.get( '/tweet/:uniqID', function(req, res, next){
+  var uniqID = Number(req.params.uniqID);
+  var list = tweetBank.find( {uniqID: uniqID} );
+  res.render( 'index', { title: 'Twitter.js — Tweet by '+ list[0].name, tweets: list } );
+});
+
 module.exports = router;
